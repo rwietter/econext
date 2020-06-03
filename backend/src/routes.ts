c96362bc6dac0b8334da0029b94ad24021518e2f
Maurício Witter <mwitter@protonmail.com>
2020-06-02 22:04:13 -0300

    feat: create routes and add static route of items

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,12 +1,18 @@
 import express from 'express';
 
+import PointsController from './controllers/PointsController';
+import ItemsController from './controllers/ItemsController';
+
+const pointController = new PointsController();
+const itemsController = new ItemsController();
+
 const routes = express.Router();
 
-const get = routes.get('/', (req, res) => {
-  res.json({
-    title: 'vikings',
-    description: 'serie',
-  });
-});
+routes.get('/items', itemsController.index);
+routes.post('/points', pointController.create);
+routes.get('/points', pointController.index);
+routes.get('/points/:id', pointController.show);
 
 export default routes;
+// .then(trx.commit)
+// .catch(trx.rollback)
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,20 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+import path from 'path';
 
 import Routes from './routes';
 
 const app = express();
+
+app.use(cors());
+
 app.use(express.json());
-app.use(Routes);
+
+app.use(Routes, (err: Response, req: Request, res: Response) => {
+  res.status(Number(err.status) || 500);
+  res.end();
+});
+
+app.use('/static', express.static(path.resolve(__dirname, '..', 'static')));
 
 app.listen(3333);
